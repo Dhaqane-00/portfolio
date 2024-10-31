423ca4a9fe1076fb9a42ae2e7fe4dfed1e3f4b80
Dhaqane-00 <Abdilaahimowliid@gmail.com>
2024-10-31 16:54:55 +0300

    new

--- app/about/_components/AboutMe.tsx
+++ app/about/_components/AboutMe.tsx
@@ -43,9 +43,9 @@ export default function AboutMe({ }: Props) {
         viewport={{ once: true }}
         className="m-8 md:w-full"
       >
-        <h1 className="text-3xl font-semibold mb-2 tracking-widest">Sobre mim</h1>
-        <p className="text-zinc-400 mb-6 text-justify md:text-left">Olá! Aqui você pode descobrir mais sobre mim, meus interesses, ferramentas que utilizo para desenvolver projetos e como pode entrar em contato comigo para uma conversa descontraída ou até mesmo discutir possíveis oportunidades de colaboração ou entrevista. Estou ansioso para compartilhar um pouco mais sobre quem sou e como posso contribuir para projetos futuros. Vamos nos conectar e explorar novas possibilidades juntos!</p>
-        <h2 className="text-blue-400 font-semibold text-xl mb-4">Informações Básicas</h2>
+        <h1 className="text-3xl font-semibold mb-2 tracking-widest">Abdilaahi Dhaqane</h1>
+        <p className="text-zinc-400 mb-6 text-justify md:text-left">Hello! Here you can find out more about me, my interests, the tools I use to develop projects and how you can get in touch with me for a relaxed chat or even discuss possible collaboration or interview opportunities. I'm looking forward to sharing a little more about who I am and how I can contribute to future projects. Let's connect and explore new possibilities together!</p>
+        <h2 className="text-blue-400 font-semibold text-xl mb-4">Basic Information</h2>
         <PersonalInfo />
       </motion.div>
       <motion.div
@@ -55,7 +55,7 @@ export default function AboutMe({ }: Props) {
         viewport={{ once: true }}
         className="m-8 md:w-full"
       >
-        <h2 className="text-blue-400 font-semibold text-xl mb-4">Olá, é um prazer te conhecer!</h2>
+        <h2 className="text-blue-400 font-semibold text-xl mb-4">Hello, it's a pleasure to meet you!</h2>
         <Image src={aboutMe} alt="Imagem da minha área de trabalho" className="rounded-xl mb-8 w-auto h-auto" />
         <p className="text-zinc-400 text-justify md:text-left mb-4">{userProps.moreAbout}</p>
         <p className="text-zinc-400 text-justify md:text-left">{userProps.aboutWork}</p>
@@ -68,18 +68,18 @@ export default function AboutMe({ }: Props) {
         className="m-8 md:w-full"
       >
         <h2 className="text-blue-400 font-semibold text-xl mb-4 flex flex-col">
-          A propósito, eu utilizo Arch linux
+          By the way, I use Arch linux
           <span className="text-zinc-400 text-xs">| {'"I use Arch, btw"'}</span>
         </h2>
         <p className="text-zinc-400 text-justify md:text-left">{userProps.aboutArch}</p>
         <div className="flex flex-col md:flex-row gap-4 mt-8">
           <div className="flex flex-col items-center justify-center">
             <Image src={archImage} alt="Imagem da minha área de trabalho" className="rounded-xl md:max-w-[425px] md:h-auto" />
-            <p className="text-zinc-600 text-xs italic text-center">Um pouco sobre meu ambiente de desenvolvimento</p>
+            <p className="text-zinc-600 text-xs italic text-center">A little about my development environment</p>
           </div>
           <div className="flex flex-col items-center justify-center">
             <Image src={archImage2} alt="Imagem da minha área de trabalho" className="rounded-xl md:max-w-[425px] md:h-auto" />
-            <p className="text-zinc-600 text-xs italic text-center">Minha área de trabalho</p>
+            <p className="text-zinc-600 text-xs italic text-center">My working area</p>
           </div>
         </div>
       </motion.div>
@@ -90,14 +90,14 @@ export default function AboutMe({ }: Props) {
         viewport={{ once: true }}
         className="m-8 md:w-full"
       >
-        <h2 className="text-blue-400 font-semibold text-xl mb-4 flex flex-col">Vamos trabalhar juntos?</h2>
+        <h2 className="text-blue-400 font-semibold text-xl mb-4 flex flex-col">Let's work together?</h2>
         <div className="flex flex-col gap-4 mt-8">
           <div className="flex flex-col gap-4 text-zinc-400 mb-8 text-justify md:text-start">
             <p>
-              À medida que continuo aprimorando minhas habilidades como desenvolvedor fullstack, estou em busca de oportunidades que me permitam aplicar minha paixão pela tecnologia e pela inovação em projetos desafiadores e impactantes. Meu objetivo não é apenas contribuir para o desenvolvimento de soluções de ponta, mas também criar experiências digitais significativas que melhorem a vida das pessoas e impulsionem o crescimento dos negócios. Estou pronto para colaborar em projetos empolgantes e para enfrentar novos desafios que me permitam expandir meus conhecimentos e habilidades.
+              As I continue to improve my skills as a fullstack developer, I am looking for opportunities that allow me to apply my passion for technology and innovation to challenging and impactful projects. My goal is not only to contribute to the development of cutting-edge solutions, but also to create meaningful digital experiences that improve people's lives and drive business growth. I am ready to collaborate on exciting projects and tackle new challenges that allow me to expand my knowledge and skills.
             </p>
             <p>
-              Se você está buscando um desenvolvedor dedicado, criativo e motivado, não hesite em entrar em contato comigo. Estou ansioso para participar de conversas envolventes sobre como podemos trabalhar juntos para alcançar resultados incríveis. Fique à vontade para usar as informações de contato abaixo para se conectar comigo. Estou aberto a oportunidades de emprego, colaborações e discussões inspiradoras sobre o mundo fascinante da tecnologia e além.
+              If you are looking for a dedicated, creative, and motivated developer, do not hesitate to contact me. I am eager to participate in engaging conversations about how we can work together to achieve incredible results. Feel free to use the contact information below to connect with me. I am open to job opportunities, collaborations, and inspiring discussions about the fascinating world of technology and beyond.
             </p>
           </div>
           <div className="grid grid-cols-2 gap-4 md:flex md:flex-row items-center justify-between w-full">
